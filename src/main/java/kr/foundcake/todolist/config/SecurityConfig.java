ea38a88476b79640d66df3d2443d44cd2caf1ddd
found-cake <skymin468@gmail.com>
2024-01-09 01:31:09 +0900

    fix wtf error Maximum sessions of 1 for this principal exceeded

--- src/main/java/kr/foundcake/todolist/config/SecurityConfig.java
+++ src/main/java/kr/foundcake/todolist/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package kr.foundcake.todolist.config;
 
 import kr.foundcake.todolist.service.MyUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @RequiredArgsConstructor
 @Configuration
@@ -20,14 +22,17 @@ public class SecurityConfig {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	@Bean
+	public static ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+		return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity security) throws Exception{
 		return security
 				.csrf(csrf -> csrf
 						.ignoringRequestMatchers(
-								"/auth/login",
-								"/auth/signup"
+								"/auth/**"
 						)
 				)
 				.authorizeHttpRequests(authorize -> authorize
@@ -47,12 +52,14 @@ public class SecurityConfig {
 				.logout(logoutConfig -> logoutConfig
 						.logoutUrl("/auth/logout")
 						.logoutSuccessUrl("/")
+						.invalidateHttpSession(true)
 				)
 				// 세션 설정
 				.sessionManagement(session -> session
 						.sessionConcurrency(concurrency -> concurrency
 								.maximumSessions(1) // 최대 한개만 생성 가능
 								.maxSessionsPreventsLogin(true) // 최대 세션 초과 불가
+								.expiredUrl("/login")
 						)
 				)
 				.userDetailsService(myUserDetailsService)
