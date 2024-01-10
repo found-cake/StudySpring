047fa6a415519a564d45d4fcbd3f5d2b3b96bd8b
found-cake <skymin468@gmail.com>
2024-01-10 18:13:48 +0900

    change tto request param

--- src/main/java/kr/foundcake/todolist/controller/AddTodoController.java
+++ src/main/java/kr/foundcake/todolist/controller/AddTodoController.java
@@ -1,12 +1,11 @@
 package kr.foundcake.todolist.controller;
 
-import kr.foundcake.todolist.dto.AddTodoRequestDto;
 import kr.foundcake.todolist.service.TodolistService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
@@ -18,8 +17,8 @@ public class AddTodoController {
 	private final TodolistService service;
 
 	@PostMapping("/add")
-	public String add(Principal principal, @ModelAttribute AddTodoRequestDto todoRequestDto) {
-		service.addTodo(principal.getName(), todoRequestDto.getTask());
+	public String add(Principal principal, @RequestParam String task) {
+		service.addTodo(principal.getName(), task);
 		return "redirect:/";
 	}
 }
